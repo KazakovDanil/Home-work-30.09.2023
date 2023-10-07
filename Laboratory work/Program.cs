46a8b359ffc07620eec8429bd8687aa934d98596
Kazak_iltlebk <kazakovdanya@gmail.com>
2023-10-07 10:21:18 +0300

    3 упр неверно

--- Laboratory work/Program.cs
+++ Laboratory work/Program.cs
@@ -74,48 +74,6 @@ namespace Exercises
             {
                 Console.WriteLine("Некоректный ввод");
             }
-
-            // Упражнение 3
-            Console.WriteLine("Упражнение 3");
-            Console.WriteLine("Эта программа рисует число в виде # и или выходит если набрано exit или закрыть");
-            Console.WriteLine("Введите число от 0 до 9 или закрыть или exit и нажмите enter: ");
-            string enterd_number = Console.ReadLine().ToLower();
-            if (byte.TryParse(enterd_number, out byte number))
-            {
-                if ((number >= 0) & (number <= 9))
-                {
-                    for (int i = 0; i < 7; i++)
-                    {
-                        if (i == 2)
-                        {
-                            Console.WriteLine("  " + String_conversion(number.ToString(), 12));
-                        }
-                        if (i == 4)
-                        {
-                            Console.WriteLine(String_conversion(number.ToString(), 12));
-                        }
-                        else
-                        {
-                            Console.WriteLine($"{String_conversion(" ", (6 - i))}{number}      {number}");
-                        }
-                    }
-                }
-                else if ((enterd_number.Equals("exit")) || (enterd_number.Equals("выход")))
-                {
-                    Console.WriteLine("Работа завершена");
-                }
-                else if ((number < 0) || (number > 9))
-                {
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.WriteLine(String_conversion(" ", 100));
-                    Thread.Sleep(3000);
-                    Console.ResetColor();
-                }
-            }
-            else
-            {
-                Console.WriteLine("Некорректный ввод");
-            }
             Console.ReadKey();
         }
     }
